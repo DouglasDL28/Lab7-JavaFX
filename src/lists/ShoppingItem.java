ff418d6c6e646fa643bf7c25a65129c46bb07417
Douglas De León M <douglasdeleon@Administradors-MacBook-Pro.local>
2018-08-29 22:37:28 -0600

    Terminé clases.

--- src/lists/ShoppingItem.java
+++ src/lists/ShoppingItem.java
@@ -1,5 +1,6 @@
 package lists;
 
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -8,15 +9,18 @@ public class ShoppingItem {
     private SimpleStringProperty name;
     private SimpleIntegerProperty quantity;
     private SimpleDoubleProperty price;
+    private SimpleBooleanProperty pending  ;
 
     public ShoppingItem (String name, Integer quantity, Double price){
         this.name = new SimpleStringProperty();
         this.quantity = new SimpleIntegerProperty();
         this.price = new SimpleDoubleProperty();
+        this.pending = new SimpleBooleanProperty();
 
         this.name.set(name);
         this.quantity.set(quantity);
         this.price.set(price);
+        this.pending.set(true);
     }
 
     //getter y setters
@@ -32,13 +36,33 @@ public class ShoppingItem {
 
     public void setPrice(Double price) {this.price.set(price);}
 
+    public void changePending () {this.pending.set(false);}
+
+    public void setPending () {this.pending.set(true);}
+
+    public Boolean getPending () {return this.pending.get();}
+
+    public Double getTotal () {
+        Double total = 0.0;
+        total += this.getQuantity() * this.getPrice();
+        return total;
+    }
+
 
     @Override
     public String toString() {
+        String state = "";
+        if (getPending()) {
+            state += "Pendiente";
+        } else {
+            state += "Comprado";
+        }
+
         return "Item: " +
-                "name= " + name +
-                ", quantity= " + quantity +
-                ", price= " + price ;
+                "nombre= " + name +
+                ", cantidad= " + quantity +
+                ", precio= " + price +
+                ", estado= " + state;
     }
 }
 
--- src/lists/ShoppingList.java
+++ src/lists/ShoppingList.java
@@ -1,22 +1,50 @@
 package lists;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class ShoppingList {
     private SimpleStringProperty name;
+    private SimpleStringProperty description;
     private SimpleListProperty<ShoppingItem> items;
+    private SimpleDoubleProperty totalPrice;
+    private SimpleIntegerProperty pendings;
 
-    public ShoppingList(String name) {
+
+    public ShoppingList(String name, String description) {
         this.items = new SimpleListProperty<>();
         this.name = new SimpleStringProperty();
+        this.description = new SimpleStringProperty();
+        this.totalPrice = new SimpleDoubleProperty();
+        this.pendings = new SimpleIntegerProperty();
 
         this.name.set(name);
+        this.description.set(description);
 
     }
 
-    public boolean hasShoppingItem(String searchName) {
+    public Double setTotalPrice () {
+        Double GrandTotal = 0.0;
         for (ShoppingItem item : items) {
+            GrandTotal += item.getTotal();
+        }
+        return GrandTotal;
+    }
+
+    public Integer countPendings () {
+        Integer pendingCount = 0;
+        for (ShoppingItem item: items) {
+            if (item.getPending()) {
+                pendingCount += 1;
+            }
+        }
+        return pendingCount;
+    }
+
+    public boolean hasShoppingItem(String searchName) {
+        for (ShoppingItem item : this.items) {
             if (item.getName() == searchName) {
                 return true;
             }
@@ -27,6 +55,12 @@ public class ShoppingList {
     public void addItem (ShoppingItem item) {
         if (!hasShoppingItem(item.getName())) {
             items.add(item);
+        }
+    }
+
+    public void deleteItem (ShoppingItem item) {
+        if (hasShoppingItem(item.getName())) {
+            items.remove(item);
         }
     }
 }
